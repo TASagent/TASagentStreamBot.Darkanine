c4825025de009b0b73ecca44772ecf5daedda320
TASagent <TASagent.Streams@gmail.com>
2022-07-14 17:36:11 -0700

    Adding command hiding

---
+++ TASagentStreamBot.Darkanine/Migrations/20220715003557_AddingCommandHiding.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TASagentStreamBot.Darkanine.Migrations
+{
+    public partial class AddingCommandHiding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Shown",
+                table: "CustomTextCommands",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Shown",
+                table: "CustomTextCommands");
+        }
+    }
+}
